8eaa485a18c7b80614d9bb334615c5c9429ad2ef
Oliver Ortega <73006272+gedazero@users.noreply.github.com>
2023-09-19 00:14:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-$(document).ready(function () {
-    
-    let URL = "https://pokeapi.co/api/v2/pokemon/";
+$(document).ready(async function () {
 
-    for (let i = 1; i <= 151; i++) {
-        fetch(URL + i)
-        .then((response) => response.json())
-        .then((data => mostrarPokemon(data)))
+
+
+    let URL = "https://pokeapi.co/api/v2/pokemon/?limit=150&offset=0";
+
+    const header = {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit    headers: {
+        'Content-Type': 'application/json'      // 'Content-Type': 'application/x-www-form-urlencoded',
     }
-    new DataTable('#pokedex');
+
+    const res = await fetch(`${URL}`, header)
+       .then((response) => response.json())
+       .then((data => data.results))
+console.log(res.map(value => fetch(value)
+    .then(res => res.json())
+    .then(res => res)
+    ));
+    // new DataTable('#pokedex');
 });
 
-function mostrarPokemon(pokemon) {
-    let tipos = pokemon.types.map((type) => `<p> Tipo ${type.type.name}</p>`);
-    tipos = tipos.join('');
-    div =
-        `
-        <tr>
-        <td>#${pokemon.id}</td>
-        <td>${pokemon.name}</td>
-        <td>${tipos}</td>
-        <td>${pokemon.height}</td>
-        <td><img src="${pokemon.sprites.other["official-artwork"].front_default}" alt=""m style="width: 300px; height: 300px;"></td>
-        </tr>
-        `;
+// function mostrarPokemon(pokemon) {
+//     let tipos = pokemon.types.map((type) => `<p> Tipo ${type.type.name}</p>`);
+//     tipos = tipos.join('');
+//     div =
+//         `
+//         <tr>
+//         <td>#${pokemon.id}</td>
+//         <td>${pokemon.name}</td>
+//         <td>${tipos}</td>
+//         <td>${pokemon.height}</td>
+//         <td><img src="${pokemon.sprites.other["official-artwork"].front_default}" alt=""m style="width: 300px; height: 300px;"></td>
+//         </tr>
+//         `;
 
-        $('#pokedex tbody').append(div);
-};
+//     $('#pokedex tbody').append(div);
+// };
